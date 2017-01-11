3a94b9229a28c8b56c8e820dea14370f19e27aa9
aatri8899 <aatri8899@withs.org>
2017-01-11 23:24:37 +0000

    Giphy Activity

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    $("#giphy").submit(function(event) {
+        event.preventDefault();
+        var query = $("#query").val();
+        console.log(query);
+        $.ajax({
+            // This is the URL of the API
+            url: "https://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=dc6zaTOxFJmzC"
+        }).done(function(response) { // the .done function happens after the API response is returned
+            console.log(response);
+
+            // this is the jQuery way to create
+            // a new image element
+            var image = $('<img>');
+            console.log(image);
+            // when we create a new element,
+            // jQuery returns an array. So we
+            // need to look for the first item
+            // in the image array
+
+            image[0].src = response.data[0].images.fixed_width_downsampled.url;
+
+            $("#results").append(image);
+            console.log(response.data.length);
+            for (var i = 0; i < response.data.length; i++) {
+                console.log(i);
+                var image2 = $('<img>');
+                image[0].src = response.data[0].images.fixed_width_downsampled.url;
+                image[0].src = response.data[1].images.fixed_width_downsampled.url;
+            }
+            
+        });
+    });
+});
